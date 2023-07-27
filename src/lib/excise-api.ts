d6ae20ddf18b79e572a073f9a570c9805aafc326
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-27 19:55:32 +0530

    fix: type error

--- src/lib/excise-api.ts
+++ src/lib/excise-api.ts
@@ -1,45 +1,63 @@
-import {Product, ProductCategory, ProductType, Vendor} from "@prisma/client";
+import { Product, ProductCategory, ProductType, Vendor } from '@prisma/client'
 
-export const authToken = "fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d"
+export const authToken = 'fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d'
 
 export function createVendorsByProductRequest(product: Product) {
-    return new Request("https://delhiexcise.gov.in/api/BrandAPI/searchBrandAvailabilityByKey", {
-        headers: {
-            "Auth-Token": authToken,
-            "BrandKey": product.externalKey.toString(),
+    return new Request(
+        'https://delhiexcise.gov.in/api/BrandAPI/searchBrandAvailabilityByKey',
+        {
+            headers: {
+                'Auth-Token': authToken,
+                BrandKey: product.externalKey.toString(),
+            },
         }
-    })
+    )
 }
 
-export const typeMap: Record<ExciseApiBrand["liquorType"], ProductType> = {
-    "Indian Liquor": ProductType.INDIAN,
-    "Foreign Liquor": ProductType.IMPORTED,
-    "Country Liquor": ProductType.COUNTRY,
+export const typeMap: Record<ExciseApiBrand['liquorType'], ProductType> = {
+    'Indian Liquor': ProductType.INDIAN,
+    'Foreign Liquor': ProductType.IMPORTED,
+    'Country Liquor': ProductType.COUNTRY,
 }
 
-export const categoryMap: Record<ExciseApiBrand["liquorCategory"], ProductCategory> = {
-    "Alcopop": ProductCategory.ALCOPOP,
-    "Beer": ProductCategory.BEER,
-    "Brandy": ProductCategory.BRANDY,
-    "Country Liquor": ProductCategory.OTHER,
-    "Draught Beer": ProductCategory.BEER,
-    "Gin": ProductCategory.GIN,
-    "Liqueur": ProductCategory.LIQUEUR,
-    "Mixed Alcoholic Beverages": ProductCategory.ALCOPOP,
-    "Rum": ProductCategory.RUM,
-    "Vodka": ProductCategory.VODKA,
-    "Whisky": ProductCategory.WHISKEY,
-    "Wine": ProductCategory.WINE,
+export const categoryMap: Record<
+    ExciseApiBrand['liquorCategory'],
+    ProductCategory
+> = {
+    Alcopop: ProductCategory.ALCOPOP,
+    Beer: ProductCategory.BEER,
+    Brandy: ProductCategory.BRANDY,
+    'Country Liquor': ProductCategory.OTHER,
+    'Draught Beer': ProductCategory.BEER,
+    Gin: ProductCategory.GIN,
+    Liqueur: ProductCategory.LIQUEUR,
+    'Mixed Alcoholic Beverages': ProductCategory.ALCOPOP,
+    Rum: ProductCategory.RUM,
+    Vodka: ProductCategory.VODKA,
+    Whisky: ProductCategory.WHISKEY,
+    Wine: ProductCategory.WINE,
 }
 
 export type ExciseApiBrand = {
     brandKey: number
     brandId: string
     brandName: string
-    liquorType: "Indian Liquor" | "Foreign Liquor" | "Country Liquor"
-    liquorCategory: "Alcopop" | "Beer" | "Brandy" | "Country Liquor" | "Draught Beer" | "Gin" | "Liqueur" | "Mixed Alcoholic Beverages" | "Rum" | "Vodka" | "Whisky" | "Wine"
+    liquorType: 'Indian Liquor' | 'Foreign Liquor' | 'Country Liquor'
+    liquorCategory:
+        | 'Alcopop'
+        | 'Beer'
+        | 'Brandy'
+        | 'Country Liquor'
+        | 'Draught Beer'
+        | 'Gin'
+        | 'Liqueur'
+        | 'Mixed Alcoholic Beverages'
+        | 'Rum'
+        | 'Vodka'
+        | 'Whisky'
+        | 'Wine'
     warehouse: string
-    status: "Y"
+    status: 'Y'
     brandAvailabilityDate: Date | null
     mrp: number
 }
@@ -47,23 +65,32 @@ export type ExciseApiBrand = {
 export type ExciseApiVendor = {
     vendId: string
     entityName: string
-    entityType: "Corporation Vend"
+    entityType: 'Corporation Vend'
     licenceId: string
     timing: string
     entityAddress: string
-    district: "CENTRAL DELHI" | "EAST DELHI" | "NEW DELHI" | "NORTH DELHI" | "NORTH EAST DELHI" | "NORTH WEST DELHI" | "SOUTH DELHI" | "SOUTH WEST DELHI" | "WEST DELHI"
-    serveForeignLiquor: "Y" | "N"
-    clFlag: "Y" | "N"
-    ilFlag: "Y" | "N"
-    flFlag: "Y" | "N"
-    liquorType: "CL" | "IL" | "IL,CL" | "IL,FL"
-    entitySubTye: "DSIIDC" | "DCCWS" | "DSCSC" | "DTTDC"
+    district:
+        | 'CENTRAL DELHI'
+        | 'EAST DELHI'
+        | 'NEW DELHI'
+        | 'NORTH DELHI'
+        | 'NORTH EAST DELHI'
+        | 'NORTH WEST DELHI'
+        | 'SOUTH DELHI'
+        | 'SOUTH WEST DELHI'
+        | 'WEST DELHI'
+    serveForeignLiquor: 'Y' | 'N'
+    clFlag: 'Y' | 'N'
+    ilFlag: 'Y' | 'N'
+    flFlag: 'Y' | 'N'
+    liquorType: 'CL' | 'IL' | 'IL,CL' | 'IL,FL'
+    entitySubTye: 'DSIIDC' | 'DCCWS' | 'DSCSC' | 'DTTDC'
     brandAvailibilty: []
 }
 
-
-
-export function mapExciseProduct(product: ExciseApiBrand): Omit<Product, "createdAt" | "updatedAt" | "id" | "image" > {
+export function mapExciseProduct(
+    product: ExciseApiBrand
+): Omit<Product, 'createdAt' | 'updatedAt' | 'id' | 'image'> {
     return {
         externalId: product.brandId,
         externalKey: product.brandKey,
@@ -71,37 +98,45 @@ export function mapExciseProduct(product: ExciseApiBrand): Omit<Product, "create
         type: typeMap[product.liquorType],
         category: categoryMap[product.liquorCategory],
         mrp: product.mrp,
-        vendorsUpdatedAt: null
+        vendorsUpdatedAt: null,
     }
 }
 
-export function mapExciseVendor(vendor: ExciseApiVendor): Omit<Vendor, "createdAt" | "updatedAt" | "id"> {
+export function mapExciseVendor(
+    vendor: ExciseApiVendor
+): Omit<
+    Vendor,
+    'createdAt' | 'updatedAt' | 'id' | 'location' | 'gmapsPlaceId'
+> {
     const productTypes: ProductType[] = []
-    if (vendor.clFlag === "Y") productTypes.push(ProductType.COUNTRY)
-    if (vendor.ilFlag === "Y") productTypes.push(ProductType.IMPORTED)
-    if (vendor.flFlag === "Y") productTypes.push(ProductType.IMPORTED)
+    if (vendor.clFlag === 'Y') productTypes.push(ProductType.COUNTRY)
+    if (vendor.ilFlag === 'Y') productTypes.push(ProductType.IMPORTED)
+    if (vendor.flFlag === 'Y') productTypes.push(ProductType.IMPORTED)
 
     return {
         externalId: vendor.vendId,
         name: vendor.entityName,
         address: vendor.entityAddress,
         productTypes,
-        entity: vendor.entitySubTye
+        entity: vendor.entitySubTye,
     }
 }
 
 export async function fetchBrands(): Promise<ExciseApiBrand[]> {
-    return fetch("https://delhiexcise.gov.in/api/BrandAPI/searchBrandByName", {
+    return fetch('https://delhiexcise.gov.in/api/BrandAPI/searchBrandByName', {
         headers: {
-            "Auth-Token": "fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d"
-        }
-    }).then(x => x.json())
+            'Auth-Token': 'fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d',
+        },
+    }).then((x) => x.json())
 }
 
 export async function fetchVendors(): Promise<ExciseApiVendor[]> {
-    return fetch("https://delhiexcise.gov.in/api/RetailVendAPI/searchRetailVendByAddress", {
-        headers: {
-            "Auth-Token": "fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d"
+    return fetch(
+        'https://delhiexcise.gov.in/api/RetailVendAPI/searchRetailVendByAddress',
+        {
+            headers: {
+                'Auth-Token': 'fb5bc83d-94ce-41c2-b9bb-9f198da0ff7d',
+            },
         }
-    }).then(x => x.json())
+    ).then((x) => x.json())
 }
