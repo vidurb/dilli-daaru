110aba5692459728e4d98d2a06bdb68579ceb593
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-28 12:47:57 +0530

    fix: only geocode vendors if necessary

--- src/app/api/sync/vendor/route.ts
+++ src/app/api/sync/vendor/route.ts
@@ -1,11 +1,18 @@
+import { Vendor } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { geocodeVendor } from '@/app/thekas/[uuid]/page'
 import { getRandomVendors } from '@/lib'
 
+async function geocodeVendorIfNecessary(vendor: Vendor) {
+    if (!vendor.gmapsPlaceId) {
+        return geocodeVendor(vendor)
+    }
+}
+
 export async function GET(request: NextRequest) {
     const vendors = await getRandomVendors(2)
-    await Promise.all(vendors.map((vendor) => geocodeVendor(vendor)))
+    await Promise.all(vendors.map((vendor) => geocodeVendorIfNecessary(vendor)))
     return NextResponse.json({ message: 'ok', vendors })
 }
 
