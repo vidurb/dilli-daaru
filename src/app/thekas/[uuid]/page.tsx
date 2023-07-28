c3fe01ef1133dbf96f1b5a4d4102f3bcfcba0232
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-28 12:38:11 +0530

    feat: enable saving location as well as place id

--- src/app/thekas/[uuid]/page.tsx
+++ src/app/thekas/[uuid]/page.tsx
@@ -1,5 +1,6 @@
 import { Client } from '@googlemaps/google-maps-services-js'
 import { Vendor } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { notFound } from 'next/navigation'
 
 import { prisma } from '@/lib/db'
@@ -14,14 +15,16 @@ export async function geocodeVendor(vendor: Vendor) {
             key: process.env.GOOGLE_MAPS_API_KEY!,
         },
     })
-    if (geocodeResponse.data.results.length >= 1) {
+    if (
+        geocodeResponse.data.status === 'OK' &&
+        geocodeResponse.data.results.length >= 1
+    ) {
         const place = geocodeResponse.data.results[0]
-        return prisma.vendor.update({
-            where: { id: vendor.id },
-            data: {
-                gmapsPlaceId: place.place_id,
-            },
-        })
+        const point = `Point(${place.geometry.location.lng} ${place.geometry.location.lat})`
+        const result = await prisma.$queryRaw<
+            Vendor[]
+        >`UPDATE "Vendor" SET "location" = ${point}::geometry, "gmapsPlaceId" = ${place.place_id} WHERE id = ${vendor.id} RETURNING "id", "externalId", "name", "address", "productTypes", "entity", "createdAt", "updatedAt", "gmapsPlaceId", "location"::text;`
+        return result[0]
     } else {
         return vendor
     }
