86509a2e8f1a8b792c42ee45b96e9baa554d7a11
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-29 22:40:19 +0530

    fix: responsive styling for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
 import Image from 'next/image'
-import Link from "next/link";
-
+import Link from 'next/link'
 
 export default async function Home() {
     return (
-        <main className={`flex min-h-screen flex-col justify-evenly p-24 items-center`}>
+        <main
+            className={`flex min-h-screen flex-col justify-evenly px-2 sm:px-0 items-center`}
+        >
             <Image
                 src="/dd-logo.svg"
                 alt="Dilli Daaru"
-                className="dark:invert pb-4"
+                className="dark:invert"
                 width={160}
                 height={24}
                 priority
             />
-            <Link href={`/daaru`} className={`bg-white px-12 py-12 rounded shadow-md text-center text-2xl uppercase w-full max-w-sm`}>Find your fix</Link>
-            <Link href={`/thekas`} className={`bg-white px-12 py-12 rounded shadow-md text-center text-2xl uppercase w-full max-w-sm`}>Check
-                local stock</Link>
+            <Link
+                href={`/daaru`}
+                className={`bg-white p-12 rounded shadow-md text-center text-2xl uppercase w-full max-w-sm`}
+            >
+                Find your fix
+            </Link>
+            <Link
+                href={`/thekas`}
+                className={`bg-white p-12 rounded shadow-md text-center text-2xl uppercase w-full max-w-sm`}
+            >
+                Check local stock
+            </Link>
         </main>
-
     )
 }
