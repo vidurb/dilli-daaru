8b315ea9362030dd4044c8c1343da3b005148097
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-30 21:04:46 +0530

    feat: update individual product page

--- src/app/daaru/[uuid]/page.tsx
+++ src/app/daaru/[uuid]/page.tsx
@@ -1,13 +1,14 @@
 import dayjs from 'dayjs'
-import Image from 'next/image'
-import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 import { fetchAndUpdateVendors } from '@/app/api/availability/route'
-import { categoryImageMap } from '@/app/daaru/product-card'
+import { categoryImageMap, ProductCard } from '@/app/daaru/product-card'
+import VendorCard from '@/app/thekas/vendor-card'
 import { prisma } from '@/lib/db'
 import { areVendorsOpen } from '@/lib/utils'
 import { translator } from '@/lib/uuid'
+
+import styles from '../daaru.module.scss'
 
 export default async function Daaru({
     params: { uuid },
@@ -16,7 +17,12 @@ export default async function Daaru({
 }) {
     const product = await prisma.product.findUnique({
         where: { id: uuid.includes('-') ? uuid : translator.toUUID(uuid) },
-        include: { vendors: true },
+        include: {
+            vendors: true,
+            _count: {
+                select: { vendors: true },
+            },
+        },
     })
     if (!product) {
         return notFound()
@@ -29,46 +35,12 @@ export default async function Daaru({
             ? await fetchAndUpdateVendors(product)
             : product.vendors
     return (
-        <main
-            className={
-                'flex min-h-screen flex-row items-center p-24 justify-center'
-            }
-        >
-            <div className="bg-white px-4 py-4 rounded shadow-sm ease-in-out duration-200 hover:shadow-md flex m-2 dark:bg-slate-900 dark:border-slate-500 dark:border">
-                <Image
-                    src={categoryImageMap[product.category]}
-                    alt={product.category}
-                    className="border rounded border-solid border-slate-300 inline mr-4"
-                    width={64}
-                    height={64}
-                    priority
-                />
-                <div>
-                    <div className="text-lg pb-1.5 leading-6 block">
-                        {product.name}
-                    </div>
-                    <div className="bg-purple-700 text-white rounded inline-block px-1.5 py-0.5 mb-0.5">
-                        {product.category}
-                    </div>
-                    <ul className={'flex flex-col'}>
-                        {vendors.map((vendor) => {
-                            return (
-                                <li
-                                    className="text-slate-500 pl-3"
-                                    key={vendor.id}
-                                >
-                                    <Link
-                                        href={`/thekas/${translator.fromUUID(
-                                            vendor.id
-                                        )}`}
-                                    >
-                                        {vendor.name}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
+        <main className={styles.mainContainer}>
+            <div className="max-w-md mx-auto">
+                <ProductCard product={product} />
+                {vendors.map((vendor, index) => (
+                    <VendorCard vendor={vendor} key={index} />
+                ))}
             </div>
         </main>
     )
--- src/app/daaru/product-card.tsx
+++ src/app/daaru/product-card.tsx
@@ -37,7 +37,7 @@ export const categoryImageMap: Record<
 export async function ProductCard({
     product,
 }: {
-    product: Product & { _count: { vendors: number } }
+    product: Product & Partial<{ _count: { vendors: number } }>
 }) {
     return (
         <div className={styles.productCard}>
@@ -59,9 +59,11 @@ export async function ProductCard({
                 <div className={styles.productCategoryPill}>
                     {product.category}
                 </div>
-                <div className={styles.productCardText}>
-                    {product._count.vendors} thekas
-                </div>
+                {product._count && (
+                    <div className={styles.productCardText}>
+                        {product._count.vendors} thekas
+                    </div>
+                )}
             </div>
         </div>
     )
