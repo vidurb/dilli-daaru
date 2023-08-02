a9f8ccf58ef89ef8e15b6f6fdbb1275c2a3de526
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-08-02 22:03:18 +0530

    fix: add other.svg, home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,33 @@ import styles from './home.module.css'
 export default async function Home() {
     return (
         <main
-            className={`flex min-h-screen flex-col justify-evenly px-2 sm:px-0 items-center`}
+            className={`flex min-h-screen flex-col gap-8 md:mx-36 sm:mx-8 items-center`}
         >
             <Image
                 src="/dd-logo.svg"
                 alt="Dilli Daaru"
-                className="dark:invert"
-                width={160}
+                className="p-4"
+                width={300}
                 height={24}
                 priority
             />
+
             <Link href={`/daaru`} className={styles.card}>
-                Find your fix
+                Find by name
+                <div class="text-slate-500 text-base pt-4">
+                    Which thekas stock the alcohol you want?
+                </div>
             </Link>
             <Link href={`/thekas`} className={styles.card}>
-                Check local stock
+                Thekas near you
+                <div class="text-slate-500 text-base pt-4">
+                    Look through what your local theka has in stock
+                </div>
             </Link>
             <HomeTheka />
+            <div class="position-fixed left-0 right-0 bottom-0">
+                made with 💛 by vidurb + gyanl
+            </div>
         </main>
     )
 }
