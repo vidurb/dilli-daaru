af35cf36beb1bf12fcb410152d8281956364f66b
Vidur Butalia <vidurbutalia@gmail.com>
2023-08-03 11:27:06 +0530

    fix: select home confirmation button

--- src/app/@modal/select-home/confirm-button.tsx
+++ src/app/@modal/select-home/confirm-button.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from 'next/navigation'
 
+import { translator } from '@/lib/uuid'
+
 export default function ConfirmHomeThekaButton({ id }: { id: string }) {
     const router = useRouter()
 
@@ -11,7 +13,7 @@ export default function ConfirmHomeThekaButton({ id }: { id: string }) {
             className="text-white bg-green-800 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
             onClick={() => {
                 window.localStorage.setItem('homeTheka', id)
-                router.back()
+                router.replace(`/thekas/${translator.fromUUID(id)}`)
             }}
         >
             Set as home
